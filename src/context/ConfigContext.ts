b4e189bce824cde54ea26c6f7c7281aaf3022e39
Davi Tanaka <davitanakaofc@gmail.com>
2024-02-01 18:18:46 -0400

    ConfigContext melhor configurado

--- src/context/ConfigContext.ts
+++ src/context/ConfigContext.ts
@@ -1,4 +1,11 @@
-import { createContext } from "react";
-const ConfigContext = createContext([]);
+import { createContext, Dispatch } from "react";
+import { ConfigContextInterface } from "@interfaces/context/ConfigContext";
+
+type ConfigContextArrayInterface = [
+  ConfigContextInterface,
+  Dispatch<ConfigContextInterface>
+]
+
+const ConfigContext = createContext<ConfigContextArrayInterface | null>(null);
 
 export { ConfigContext };
