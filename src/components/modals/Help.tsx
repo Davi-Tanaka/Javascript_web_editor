4446c3345de0c7c2df68f210f5bad6696e3a5b7e
Davi Tanaka <davitanakaofc@gmail.com>
2024-02-01 18:20:23 -0400

    #

--- src/components/modals/Help.tsx
+++ src/components/modals/Help.tsx
@@ -1,7 +1,13 @@
 import ModalTemplate from "@components/modals/ModalTemplate";
 import Sidebar from "@components/navigation/Sidebar";
 import Item from "@components/navigation/Item";
-import { useCallback, useMemo } from "react";
+
+import styles from "@styles/components/modals/ModalContent.module.scss";
+import tableStyles from "@styles/components/CombinationTable.module.scss"
+
+import colors from "@styles/utils/_colors.module.scss";
+
+import { useCallback, useMemo, Fragment } from "react";
 
 interface HelpContent {
   title: string,
@@ -11,42 +17,68 @@ interface HelpContent {
 function HelpModal({ closeFunc }) {
   const values = [
     {
-      title: "Como contribuir ?",
-      content: "Abra um issue ou copie o <a href='https://github.com/Davi-Tanaka/Javascript_web_editor'>projeto</a> para seu github e faça um PR com suas melhorias."
+      title: "Combinações de teclas.",
+      content: (
+        <Fragment>
+          <table className={tableStyles.table}>
+            <tr>
+              <th>Combinação</th>
+              <th>Ação</th>
+            </tr>
+            <tr>
+              <td>Escape</td>
+              <td>Fecha todos os modals e popup.</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </Fragment>
+      )
     }
   ]
 
   const formatElementId = useCallback((str: string) =>  {
-    const finalValue = str.trim().toLowerCase().replace(" ", "-");
-
-    return finalValue;
+    const finalValue = str.trim().toLowerCase().replace(/\s+/g, "-");
+    return "#" + finalValue;
   }, []);
 
+  var navbar = {
+    w: "10em"
+  }
+  
   return(
     <>
       <ModalTemplate
         title="Ajuda"
         functionToCloseTheModel={closeFunc}
+        width="clamp(30em, 80vw, 80em)"
       >
       <Sidebar
-        width="10em"
-        >
+        width={navbar.w}
+      >
           {
             values.map(value => <Item value={value.title} goTo={formatElementId(value.title)}/>)
           }
       </Sidebar>
 
-      <div className="modal-content">
+      <div
+        className={styles.modal_content}
+        style={{
+          width: `calc(100% - ${navbar.w})`
+        }}
+      >
           {
             values.map(value => {
               return(
                 <>
-                  <div className="modal-section">
-                    <div className="wrapper-title" id={formatElementId(value.title)}>
-                        <h5>{ value.title }</h5>
+                  <div className={styles.modal_section}>
+                    <div className={styles.wrapper_title} id={formatElementId(value.title)}>
+                        <h5 className={styles.title}>{ value.title }</h5>
                     </div>
-                    <div className="section-content" id="autor_help_modal">
-                      <p>
+                    <div className={styles.section_content} id="autor_help_modal">
+                      <p className={styles.paragraph}>
                         { value.content }
                       </p>
                     </div>
